3acd722dfa527c09e2b44bbb94d7a9c62ae59c82
Javier Pérez <javierperezherrera@gmail.com>
2021-07-29 18:01:09 -0400

    Se agrega Desafio del NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function App () {
+    return <>
+        <h1>Laraema - Tienda de Mascotas</h1>
+        <div>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Todo para Perros</a></li>
+                <li><a href="#">Todo para Gatos</a></li>
+                <li><a href="#">Todo para Roedores</a></li>
+                <li><a href="#">Preguntas Frecuentes</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        <h2>OFERTAS! Solo por esta semana</h2>
+        </div>
+    </>;
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import NavBar from './components/NavBar'
+
 
 ReactDOM.render (
     <div>
-        <h1>Tienda de Mascotas Laraema</h1>
+        <NavBar />
         <h2>La Tienda No. 1 en productos exclusivos</h2>
-        <h3>Nuevos Productos</h3>
         <p>Lorem Ipsum</p>
         <h3>Ofertas Exclusivas</h3>
         <p>Lorem Ipsum</p>
