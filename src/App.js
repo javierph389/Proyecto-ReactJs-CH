9ffbea6d4cab5b253bcfc2fa10fe8e3a5de1d025
Javier Pérez <javierperezherrera@gmail.com>
2021-07-29 20:23:34 -0400

    Desafio de Clase 3, se agrega navbar y css

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import NavBar from './components/NavBar'
+
+
+ReactDOM.render (
+    <div>
+        <NavBar />
+        <h2>La Tienda No. 1 en productos exclusivos</h2>
+        <p>Lorem Ipsum</p>
+        <h3>Ofertas Exclusivas</h3>
+        <p>Lorem Ipsum</p>
+    </div>,
+    document.getElementById('root'));
--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import NavBar from './components/NavBar'
+import App from './components/NavBar'
 
 
 ReactDOM.render (
-    <div>
-        <NavBar />
-        <h2>La Tienda No. 1 en productos exclusivos</h2>
-        <p>Lorem Ipsum</p>
-        <h3>Ofertas Exclusivas</h3>
-        <p>Lorem Ipsum</p>
-    </div>,
-    document.getElementById('root'));
+    <App />,
+    document.getElementById('root')
+);
