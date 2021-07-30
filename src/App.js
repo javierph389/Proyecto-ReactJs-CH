fa51462202999ded0f3fa02233ac0122fb2148ee
Javier Pérez <javierperezherrera@gmail.com>
2021-07-29 20:44:26 -0400

    Entrega de Desafio de Clase 3 ReactJs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
 import NavBar from './components/NavBar'
 
-
-ReactDOM.render (
-    <div>
+const App = () => {
+    return(<div>
         <NavBar />
-        <h2>La Tienda No. 1 en productos exclusivos</h2>
-        <p>Lorem Ipsum</p>
-        <h3>Ofertas Exclusivas</h3>
-        <p>Lorem Ipsum</p>
-    </div>,
-    document.getElementById('root'));
+        <div>
+            <h2>La Tienda No. 1 en productos exclusivos</h2>
+            <p>Lorem Ipsum</p>
+            <h3>Ofertas Exclusivas</h3>
+            <p>Lorem Ipsum</p>
+        </div>
+    </div>)
+}
+
+export default App 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.css'
 
-export default function App () {
+export default function NavBar () {
     return <>
         <div className="barranav">
         <h1 className="titulo">Emalara <br />La Tienda de Mascotas</h1>
